8d5d3a45aa152e7496086bff0bf226b4c4398466
XiaoPeng <zxpcs0x3@gmail.com>
2025-03-26 21:37:52 +0800

    Test:test

--- src/test/java/com/zxp/nowcodercommunity/NowcoderCommunityApplicationTests.java
+++ src/test/java/com/zxp/nowcodercommunity/NowcoderCommunityApplicationTests.java
@@ -1,13 +1,24 @@
 package com.zxp.nowcodercommunity;
 
+import com.zxp.nowcodercommunity.util.EmailSendService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NowcoderCommunityApplicationTests {
 
+    @Autowired
+    EmailSendService emailSendService;
+
     @Test
     void contextLoads() {
+        System.out.println("Hello");
+    }
+
+    @Test
+    void emailSendTest() {
+        emailSendService.sendSimpleMail("3436854592@qq.com", "A Test Email From SpringBoot Auto Send", "Hello,World!");
     }
 
 }
