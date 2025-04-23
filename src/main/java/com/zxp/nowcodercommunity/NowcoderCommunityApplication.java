8b3cef36fbcd6f2d300c75ec0e971f8065c14d94
XiaoPeng <zxpcs0x3@gmail.com>
2025-04-23 21:00:03 +0800

    config:解决netty端口冲突问题

--- src/main/java/com/zxp/nowcodercommunity/NowcoderCommunityApplication.java
+++ src/main/java/com/zxp/nowcodercommunity/NowcoderCommunityApplication.java
@@ -1,10 +1,17 @@
 package com.zxp.nowcodercommunity;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NowcoderCommunityApplication {
+
+    @PostConstruct
+    public void init() {
+        // 解决netty启动冲突问题
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NowcoderCommunityApplication.class, args);
